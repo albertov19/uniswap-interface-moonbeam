5a3743076f6cde410eb3e424a451b4f7e973126e
albertov19 <albertoviera@gmail.com>
2021-02-22 16:45:54 +0000

    first commit

---
+++ src/constants/multicall/index.ts
@@ -0,0 +1,10 @@
+import { ChainId } from 'moonbeamswap/sdk'
+import MULTICALL_ABI from './abi.json'
+
+const MULTICALL_NETWORKS: { [chainId in ChainId]: string } = {
+  [ChainId.MAINNET]: '0xeefBa1e63905eF1D7ACbA5a8513c70307C1cE441',
+  [ChainId.STANDALONE]: '0xF8cef78E923919054037a1D03662bBD884fF4edf',
+  [ChainId.MOONBASE]: '0xD7bA481DE7fB53A7a29641c43232B09e5D9CAe7b'
+}
+
+export { MULTICALL_ABI, MULTICALL_NETWORKS }
---
+++ src/utils/currencyId.ts
@@ -0,0 +1,7 @@
+import { Currency, DEV, Token } from 'moonbeamswap/sdk'
+
+export function currencyId(currency: Currency): string {
+  if (currency === DEV) return 'ETH'
+  if (currency instanceof Token) return currency.address
+  throw new Error('invalid currency')
+}
